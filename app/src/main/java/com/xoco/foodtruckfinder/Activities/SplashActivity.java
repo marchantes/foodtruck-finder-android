82b33397ef9fb8efc0409349a9c04a1a2e153cec
uliseschino <ulises.chino@gmail.com>
2015-12-02 12:04:45 -0600

    feature: Add home screen
    Add Home activity which holds home screen. Home screen has two bottons, if user click on Go To Map, app will redirect user to Map/Main Activity. User also has the option to register. This activity appears immediately aflter app splash.

--- app/src/main/java/com/xoco/foodtruckfinder/Activities/SplashActivity.java
+++ app/src/main/java/com/xoco/foodtruckfinder/Activities/SplashActivity.java
@@ -1,9 +1,11 @@
-package com.xoco.foodtruckfinder;
+package com.xoco.foodtruckfinder.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+
+import com.xoco.foodtruckfinder.R;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -18,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashActivity.this, MapActivity.class);
+                Intent mainIntent = new Intent(SplashActivity.this, HomeActivity.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
